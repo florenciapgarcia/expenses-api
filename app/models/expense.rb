1766b54f99e7417b9bd0aabf16bc7e33ff99935a
Florencia Pezcara <fpezcara@gmail.com>
2023-03-18 17:49:07 +0000

    Add Expense model validations for title, amount in cents and date

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,15 @@
 class Expense < ApplicationRecord
+  validates :title,
+            presence: {
+              message: 'Please enter a title for your expense',
+            }
+  validates :amount_in_cents,
+            presence: {
+              message: 'Please enter an amount in cents',
+            },
+            numericality: {
+              only_integer: true,
+              greater_than: 0,
+            }
+  validates :date, presence: { message: 'Please set a date' }
 end
