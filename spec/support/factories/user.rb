321527333d17ab330abfe1dac120a544e9604e21
fpezcara <fpezcara@gmail.com>
2023-05-14 14:35:15 +0100

    Add Faker gem

--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -1,8 +1,10 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    first_name { "Pebbles" }
-    last_name { "Test" }
-    email { "pebbs_test@test.com" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     password { "password" }
     password_confirmation { "password" }
   end
